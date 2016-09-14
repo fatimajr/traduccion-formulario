a5df01744e2dc3ba506dff4985a7c27119720df1
fatima <fatima.jacoboruiz@gmail.com>
2016-09-14 17:20:09 -0500

    traduccion - formulario1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 document.getElementById("form-signin-heading").innerHTML = "Por favor inicia sesión";
 document.getElementById("inputEmail").placeholder = "Correo Electrónico";
 document.getElementById("inputPassword").placeholder = "Contraseña";
-document.querySelector("span").innerHTML = "Correo Electrónico";
 document.querySelector("span").innerHTML = "Recordar datos";
-document.querySelector("button").innerHTML = "Iniciar Sesiòn";
+document.querySelector("button").innerHTML = "Iniciar Sesión";
